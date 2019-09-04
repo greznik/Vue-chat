cae2cf3121452e43488e35949e751f91258ee046
Reznx <48242089+Reznx@users.noreply.github.com>
2019-09-04 10:20:43 +0500

    Connection server

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-btn @click="message">New message</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  sockets: {
+    connect() {
+      console.log("Connected client");
+    }
+  },
+  methods: {
+    message() {
+      this.$socket.emit('createMessage', {
+        text: 'Client message recived AXAXA'
+      })
+    }
+  }
+};
+</script>
